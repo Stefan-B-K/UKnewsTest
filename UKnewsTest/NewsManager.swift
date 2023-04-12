d4d5a24f8b405d0e47992bcc7aabfbca5fe48875
iStef <stefan_b_k@icloud.com>
2023-04-12 13:49:30 +0300

    final

--- UKnewsTest/NewsManager.swift
+++ UKnewsTest/NewsManager.swift
@@ -5,7 +5,10 @@ import UIKit
 @MainActor
 final class NewsManager: ObservableObject {
   
+  private let newsApiKey = Bundle.main.infoDictionary?["NewsApiKey"] as! String
+  
   @Published var news = [NewsModel]()
+  
   
   static let defaultImageUrl = "https://thumbs.dreamstime.com/b/news-newspapers-folded-stacked-word-wooden-block-puzzle-dice-concept-newspaper-media-press-release-42301371.jpg"
   
@@ -17,7 +20,7 @@ final class NewsManager: ObservableObject {
   func loadNews(country: String) {
     Task {
       do {
-        let urlString = "https://newsapi.org/v2/top-headlines?country=\(country)&apiKey=347ea76b36f4455b85cf7afe8b84ea74"
+        let urlString = "https://newsapi.org/v2/top-headlines?country=\(country)&apiKey=\(newsApiKey)"
         let url = URL(string: urlString)!
         let (data, _) = try await URLSession.shared.data(from: url)
         self.news = try JSONDecoder().decode(NewsAPI.self, from: data).articles
